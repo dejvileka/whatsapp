4a0bce216c3ee05ea50fd31fc1ba3dceb54cba2e
Dejvid Leka <dejvid@themobilelife.com>
2022-09-23 14:21:53 +0200

    initial commit

---
+++ app/src/main/java/com/dejvileka/whatsapp/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.dejvileka.whatsapp.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
